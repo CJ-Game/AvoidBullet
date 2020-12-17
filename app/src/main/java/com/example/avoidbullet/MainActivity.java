ef2310f247eebc067ca53993204a3ea3d89908f8
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2020-12-17 21:33:08 +0900

    KeyDownAgain

--- app/src/main/java/com/example/avoidbullet/MainActivity.java
+++ app/src/main/java/com/example/avoidbullet/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.avoidbullet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.start_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,GameActivity.class));
+            }
+        });
     }
 }
