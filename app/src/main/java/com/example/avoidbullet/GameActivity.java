477cd1ebba47cbc0d9b0dc48cf81803f808dfbcc
U-WangE <chamchamm@naver.com>
2020-12-18 00:18:33 +0900

    chan

--- app/src/main/java/com/example/avoidbullet/GameActivity.java
+++ app/src/main/java/com/example/avoidbullet/GameActivity.java
@@ -10,25 +10,27 @@ import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
-import android.widget.ImageView;
 
 import com.zerokol.views.joystickView.JoystickView;
 
 public class GameActivity extends AppCompatActivity {
+    // 타이머
     static int msec = 0;
     static String msec_format;
     static String sec_format;
     static String min_format;
     static TextView alive_time;
     static TextView alive_millietime;
+
+    // 조이스틱
     private JoystickView joystick;
-    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+    // 조이스틱
         joystick = (JoystickView) findViewById(R.id.joystickView);
         //조이스틱 움직이기
         joystick.setOnJoystickMoveListener(new JoystickView.OnJoystickMoveListener() {
@@ -38,28 +40,28 @@ public class GameActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 switch (direction) //조이스틱이 움직이는 방향에 따라 imageview 움직이기
                 {
-                    case JoystickView.FRONT:
+                    case JoystickView.FRONT:  // 위
                         findViewById(R.id.character).setY(findViewById(R.id.character).getY() - power / 5);
                         break;
                     case JoystickView.FRONT_RIGHT:
                         findViewById(R.id.character).setX(findViewById(R.id.character).getX() - power / 5);
                         findViewById(R.id.character).setY(findViewById(R.id.character).getY() - power / 5);
                         break;
-                    case JoystickView.RIGHT:
+                    case JoystickView.RIGHT:  // 오른쪽
                         findViewById(R.id.character).setX(findViewById(R.id.character).getX() - power / 5);
                         break;
                     case JoystickView.RIGHT_BOTTOM:
                         findViewById(R.id.character).setX(findViewById(R.id.character).getX() - power / 5);
                         findViewById(R.id.character).setY(findViewById(R.id.character).getY() + power / 5);
                         break;
-                    case JoystickView.BOTTOM:
+                    case JoystickView.BOTTOM:  // 아래
                         findViewById(R.id.character).setY(findViewById(R.id.character).getY() + power / 5);
                         break;
                     case JoystickView.BOTTOM_LEFT:
                         findViewById(R.id.character).setX(findViewById(R.id.character).getX() + power / 5);
                         findViewById(R.id.character).setY(findViewById(R.id.character).getY() + power / 5);
                         break;
-                    case JoystickView.LEFT:
+                    case JoystickView.LEFT:  // 왼쪽
                         findViewById(R.id.character).setX(findViewById(R.id.character).getX() + power / 5);
                         break;
                     case JoystickView.LEFT_FRONT:
@@ -72,19 +74,21 @@ public class GameActivity extends AppCompatActivity {
 
             }
         }, 15);  //loopInterval
+
+     //타이머
         alive_time = findViewById(R.id.alive_time);
         alive_millietime = findViewById(R.id.alive_millietime);  // 아래 첨자 밀리초
 
-        final Handler handler = new Handler() {
+        final Handler handler = new Handler() {  // 없으면 UI 변경 못함
             public void handleMessage(Message msg) {
                 msec++;
                 msec_format = String.format("%02d", msec % 100);
                 sec_format = String.format("%02d", (msec / 100) % 60);
                 min_format = String.format("%02d", ((msec / 100) / 60));
 
-                if (msec / 100 / 60 == 0) {  //
+                if (msec / 100 / 60 == 0) {  // 초:밀리초
                     alive_time.setText(sec_format + " : " + msec_format);
-                } else {
+                } else {  // 분:초
                     alive_millietime.setVisibility(View.VISIBLE);
                     alive_time.setText(min_format + " : " + sec_format);
                     alive_millietime.setText("." + msec_format);
@@ -100,7 +104,7 @@ public class GameActivity extends AppCompatActivity {
                 handler.sendMessage(msg);
             }
         };
-        timer.schedule(t, 0, 10);
+        timer.schedule(t, 0, 10); // 10밀리초 마다 run
     }
 }
 
